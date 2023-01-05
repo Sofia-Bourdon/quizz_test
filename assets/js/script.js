6f276a91d33b79db246000ad1fae9f76163f326e
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-01-05 18:05:15 +0000

    button styling and start of js functions

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const startButton = document.getElementById('start-btn')
+const questionBox = document.getElementById('question-box1')
+
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+    console.log("quiz started");
+
+}
+
+function nextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
