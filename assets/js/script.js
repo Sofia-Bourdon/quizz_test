dcf0a36c53fc92286d308e05e428ad09af732fb6
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-01-09 19:57:58 +0000

    changes in html file and setting questions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,68 @@ startButton.addEventListener('click', startGame)
 
 function startGame(){
     console.log("quiz started");
-    startBox.classList.add('hide')
-    questionBox.classList.remove('hide')
+    window.location.href = "quiz.html";
+}
+
+const QUESTIONS = [
+    {
+      id: 1,
+      questionText: 'Can you name this vegetable?',
+      options: [
+        'Amaranth',
+        'Romanesco',
+        'Oca',
+        'Nopal'
+      ],
+      correctAnswer: 'Romanesco'
+    },
+    {
+      id: 2,
+      questionText: 'What is this dish called?',
+      options: [
+        'Lo Mein',
+        'Udon soup',
+        'Pho',
+        'Beef Ramen'
+      ],
+      correctAnswer: 'Pho'
+    },
+    {
+      id: 3,
+      questionText: 'Which cut is this?',
+      options: [
+        'Ribeye',
+        'Sirloin',
+        'Rump steak',
+        'Porterhouse'
+      ],
+      correctAnswer: 'Ribeye'
+    },
+    {
+        id: 4,
+        questionText: 'What is this fruit called?',
+        options: [
+          'Blueberry',
+          'Acai',
+          'Boysenberry',
+          'Elderberry'
+        ],
+        correctAnswer: 'Acai'
+      },
+      {
+        id: 5,
+        questionText: 'What is this dish called?',
+        options: [
+          'Mille-feuille',
+          'Clafoutis',
+          'Rum cake',
+          'Baklava'
+        ],
+        correctAnswer: 'Baklava'
+      }
+  ];
+  
+function setQuestion(){
 
 }
 
