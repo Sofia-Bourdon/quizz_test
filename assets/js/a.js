4de76c082e5d422f56b79811e8ab82c56fef5dc7
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-01-16 20:28:11 +0000

    js changes

---
+++ assets/js/a.js
@@ -0,0 +1,10 @@
+const startButton = document.getElementById('start-btn');
+
+
+function startGame(){
+  console.log("quiz started");
+  window.location.href = "quiz.html";
+}
+
+
+startButton.addEventListener('click', startGame);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,4 @@
-const startButton = document.getElementById('start-btn');
-let resultBox = document.getElementById('result-box')
-
-
-if (startButton){
-
-  startButton.addEventListener('click', startGame)
-
-  function startGame(){
-    console.log("quiz started");
-    window.location.href = "quiz.html";
-  }
-}
-
-
-  const QUESTIONS = [
+const QUESTIONS = [
       {
         questionText: 'Can you name this vegetable?',
         image : "assets/images/veg-romanesco.jpeg",
@@ -71,63 +56,90 @@ if (startButton){
         }
     ];
 
-
+  let resultBox = document.getElementById('result-box')
+  
   var optionButtons = document.querySelectorAll('.option-button');
   let questionElement = document.getElementById('question');
+  let image = document.getElementById('img_question')
+  let nextButton = document.getElementById('next-btn');
+  const currentQuestionIndex = 0;
 
 
   function showQuestion(){
 
   // replacing the question text content
-    questionElement.textContent = QUESTIONS[0].questionText;
+  // questionElement.textContent = QUESTIONS[0].questionText;
+  questionElement.textContent = QUESTIONS[currentQuestionIndex].questionText;
   
   // replacing the options content with the content inside the array 
       optionButtons.forEach(function(element, index){
-      element.textContent = QUESTIONS[0].options[index];
-
+      element.textContent = QUESTIONS[currentQuestionIndex].options[index];
     })
-
+  // show image
+  image.src = QUESTIONS[currentQuestionIndex].image;
   }
-  showQuestion();
+  // showQuestion();
 
+  // function selectAnswer(){
+    // console.log("answer selected");
+    // this.style.backgroundColor = "lightgray"
+  // }
 
-  function selectAnswer(){
-    console.log("answer selected");
-    this.style.backgroundColor = "lightgray"
-  }
+  // for (let i = 0; i < optionButtons.length; i++) {
+    // optionButtons[i].addEventListener('click', selectAnswer);
+  // }
 
-  for (let i = 0; i < optionButtons.length; i++) {
-    optionButtons[i].addEventListener('click', selectAnswer);
-  }
-
-
-  let image = document.getElementById('img_question')
-  let nextButton = document.getElementById('next-btn');
-
-  if (nextButton) {
+  // if (nextButton) {
   // changing the question and option content when the next button is clicked
   
-    nextButton.addEventListener('click', setNextQuestion)
+    // nextButton.addEventListener('click', setNextQuestion)
 
-    function setNextQuestion(){
+    // function setNextQuestion(){
+      // console.log("next question ready!");
+      // }
+      // let random = Math.floor(Math.random() * QUESTIONS.length);
+      // questionElement.textContent = QUESTIONS[random].questionText;
 
-      console.log("next question ready!");
+    // image.src = QUESTIONS[random].image;
 
-      // questionElement.textContent = QUESTIONS[1].questionText;
-      // image.src = QUESTIONS[1].image;
-      // optionButtons.forEach(function(element, index){
-      // element.textContent = QUESTIONS[1].options[index];
+    // optionButtons.forEach(function(element, index){
+      // element.textContent = QUESTIONS[random].options[index];
       // })
 
-      let random = Math.floor(Math.random() * QUESTIONS.length);
+      // }
+  // }
 
-      questionElement.innerText = QUESTIONS[random].questionText;
-
-      image.src = QUESTIONS[random].image;
-
-      optionButtons.forEach(function(element, index){
-      element.textContent = QUESTIONS[random].options[index];
-      })
-
-      }
-}
+  function onOptionClick(event){
+    console.log("answer selected");
+    this.style.backgroundColor = "lightgray";
+    const selectedAnswer = event.target.textContent;
+    // Also check if answer is correct
+    if (selectedAnswer ===  QUESTIONS[currentQuestionIndex].correctAnswer) {
+      // INcrement the score
+      console.log("Correct");
+    } else {
+      console.log("InCorrect");
+    }
+  }
+  
+  function onNextClick(){
+    console.log("next question ready!");
+    currentQuestionIndex++;
+    showQuestion();
+  }
+  
+  function initEventListeners() {
+    for (let i = 0; i < optionButtons.length; i++) {
+      optionButtons[i].addEventListener('click', onOptionClick);
+    }
+  
+    nextButton.addEventListener('click', onNextClick);
+  }
+  
+  function onPageLoad() {
+    initEventListeners();
+    showQuestion();
+  }
+  
+  
+  document.onload = onPageLoad;
